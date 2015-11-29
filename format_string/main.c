20fb98aa3b43bee80585034b8222ccaed29aebee
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-11-29 23:32:18 +0100

    Add format string exploit

---
+++ format_string/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char passwd[100] = "AAAABBBB";
+    char buf[100] = {0};
+
+    scanf("%s", buf);
+
+    if (strncmp(buf, passwd, 100) == 0) {
+        printf("correct\n");
+    } else {
+        printf("passwd: %s\n", passwd);
+        printf("You entered:\n");
+        printf(buf);
+        printf("\n");
+    }
+
+    return 0;
+}
