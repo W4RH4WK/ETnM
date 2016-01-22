6050da18a62062e7d5470a9fbde1609d244fe22e
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-01-22 12:12:29 +0100

    Paper: Draft ROP, ASLR and StackGuard

---
+++ src/return_oriented_programming/rop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+char *not_used = "/bin/sh";
+
+void not_called(void) {
+    puts("Not quite a shell...");
+    system("/bin/date");
+}
+
+void vulnerable_function(char* string) {
+    char buffer[100] = {0};
+    strcpy(buffer, string);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        vulnerable_function(argv[1]);
+    }
+    return 0;
+}
